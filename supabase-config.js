4c9cee744352edb69285f28666f8e69be05e1423
José Carlos Millán de Cortés <josecarlosmillandecortes@Tsuki-4.local>
2026-04-05 00:40:05 +0200

    Fix: Error de inicialización con const supabase en el frontend

--- supabase-config.js
+++ supabase-config.js
@@ -8,7 +8,7 @@ const SUPABASE_URL = 'https://errspjsarhkqexanostz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVycnNwanNhcmhrcWV4YW5vc3R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwNjEzNDgsImV4cCI6MjA5MDYzNzM0OH0.Cs-VzZVnYz614Ogg9DHy-3mEkCXRzq9uMxninyCFv9w';
 
 // Inicialización del cliente Supabase desde el CDN
-// Esto estará disponible de forma global en toda la web gracias al CDN <script> en HTML
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Sobreescribimos el objeto global window.supabase pasándole sus credenciales para que actúe como cliente
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 console.log("Supabase Cliente Inicializado correctamente (esperando consultar la BD...)");
